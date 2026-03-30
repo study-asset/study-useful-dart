3c7bc6002bd1accbad651517751b4bc9bd6fd288
Jiwoo So <jiwooproity@naver.com>
2026-03-30 14:39:09 +0900

    Feat(extension/string.dart): Added 'toTitleCase' and 'repeat' functions, converts to first letter of each word to upper case and repeats words based on the given count

--- extension/string.dart
+++ extension/string.dart
@@ -1,8 +1,16 @@
 extension StringExtension on String {
   String get capitalize => isNotEmpty ? '${this[0].toUpperCase()}${substring(1)}' : '';
+
+  String toTitleCase() {
+    return split(" ").map((word) => word.isNotEmpty ? '${word[0].toUpperCase()}${word.substring(1)}' : '').join(' ');
+  }
+
+  String repeat(int count) => List.filled(count, this).join(" ");
 }
 
 void main() {
   String name = 'john wick';
   print(name.capitalize); // John wick
+  print(name.toTitleCase()); // John Wick
+  print(name.repeat(10)); // john wick john wick john wick john wick .. +6
 }
